467b30090978a7c4ee0866edb8b263b34ba13a19
casey-stpierre <cjstpierre19@gmail.com>
2021-10-15 00:11:08 -0400

    Build collaborative filtering system and did some polishing.

--- MovieRecommendationSystemRScript.R
+++ MovieRecommendationSystemRScript.R
@@ -134,7 +134,7 @@ sum(rowMeans(normalized_ratings) > 0.00001)
 
 image(normalized_ratings[rowCounts(normalized_ratings) > minimum_movies,
                          colCounts(normalized_ratings) > minimum_users],
-      main = "Normalozed Ratings of the Top Users")
+      main = "Normalized Ratings of the Top Users")
 
   #Binarizing the data. Rating above a 3=1, anything below a 3=0
 binary_minimum_movies <- quantile(rowCounts(movie_ratings), 0.95)
@@ -143,3 +143,62 @@ binary_minimum_users <- quantile(colCounts(movie_ratings), 0.95)
 good_rated_films <- binarize(movie_ratings, minRating = 3)
 image(good_rated_films[rowCounts(movie_ratings) > binary_minimum_movies, colCounts(movie_ratings) > binary_minimum_users],
       main = "Heatmap of the top users and movies")
+
+  #Developing an item based collaborative filtering system
+  #The algorithm is going to build a similar-items table to find similarities in the movies based on the user's ratings
+  #I'm splitting the dataset into 80% training data and 20% test set
+sampled_data<- sample(x = c(TRUE, FALSE),
+                      size = nrow(movie_ratings),
+                      replace = TRUE,
+                      prob = c(0.8, 0.2))
+training_data <- movie_ratings[sampled_data, ]
+testing_data <- movie_ratings[!sampled_data, ]
+
+  #I'm checking out some parameters of the system, using the cosine method to do so
+recommendation_system <- recommenderRegistry$get_entries(dataType ="realRatingMatrix")
+recommendation_system$IBCF_realRatingMatrix$parameters
+
+recommen_model <- Recommender(data = training_data,
+                              method = "IBCF",
+                              parameter = list(k = 30))
+recommen_model
+class(recommen_model)
+
+model_info <- getModel(recommen_model)
+class(model_info$sim)
+dim(model_info$sim)
+top_items <- 20
+image(model_info$sim[1:top_items, 1:top_items],
+      main = "Heatmap of the first rows and columns")
+
+sum_rows <- rowSums(model_info$sim > 0)
+table(sum_rows)
+sum_cols <- colSums(model_info$sim > 0)
+qplot(sum_cols, fill=I("steelblue"), col=I("red"))+ ggtitle("Distribution of the column count")
+
+  #Building a recommender system on the dataset 
+  #Creating a variable and using the predict function to rank the film
+  #Each rating will carry a weight which will be multiplied by its related similarities
+top_recommendations <- 10
+predicted_recommendations <- predict(object = recommen_model,
+                               newdata = testing_data,
+                               n = top_recommendations)
+predicted_recommendations
+
+user1 <- predicted_recommendations@items[[1]]
+movies_user1 <- predicted_recommendations@itemLabels[user1]
+movies_user2 <- movies_user1
+for (index in 1:10) {
+  movies_user2[index] <- as.character(subset(movie_data,
+                                             movie_data$movieId == movies_user1[index])$title)
+}
+movies_user2
+
+recommendation_matrix <- sapply(predicted_recommendations@items,
+                                function(x){ as.integer(colnames(movie_ratings)[x]) })
+recommendation_matrix[,1:4]
+
+  #Visualizing the distribution
+number_of_items <- factor(table(recommendation_matrix))
+chart_title <- "Distribution of the Number of Items for IBCF"
+qplot(number_of_items, fill=I("steelblue"), col=I("red"))+ ggtitle(chart_title)
